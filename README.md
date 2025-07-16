178f6042c1a912e59b65d1e1e083400caffa0ac4
Kelvin Nguyen <keltamnguyen@yahoo.com>
2025-07-15 20:00:30 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Webpack Template
+A starter template for building web apps with Webpack 5.
+
+## Features/Plugins
+- HTML Webpack Loader
+- Style Loader
+- CSS Loader
+- HTML Loader
+- Webpack Dev Server
+
+## Scripts
+```npm run build```: Use Webpack to build the project in production mode.
+```npm run dev```: Open Webpack Dev Server in development mode.
+```npm run deploy```: Push the output of build script into a branch called "gh-pages" for deployment on Github Pages. (Make sure branch is set to "gh-pages" in Settings > Pages in the Github repository).
+
+## Installation
+1. Run ```npm install``` to install required dependencies.
---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[name][ext]',
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+              test: /\.css$/i,
+              use: ["style-loader", "css-loader"],
+              generator: {
+                filename: '[name][ext]'
+              }
+            },
+            {
+              test: /\.html$/i,
+              loader: "html-loader",
+            },
+            {
+              test: /\.(png|svg|jpg|jpeg|gif)$/i,
+              type: 'asset/resource',
+              generator: {
+                  filename: 'assets/[name][ext]'
+              }
+            },
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+              type: 'asset/resource',
+              generator: {
+                  filename: 'assets/fonts/[name][ext]'
+              }
+            }
+        ]
+    }
+}
